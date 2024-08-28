d7627ab34d839f26a13ca4df96df0a9a95c0941b
guio0228 <93069491+guio0228@users.noreply.github.com>
2024-08-28 22:20:24 +0800

    update

--- app/Jobs/TestJJob.php
+++ app/Jobs/TestJJob.php
@@ -2,8 +2,8 @@
 
 namespace App\Jobs;
 
-use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
