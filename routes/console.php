0dbbb74c73c772e39f88af86c28d96c81000aa1a
guio0228 <93069491+guio0228@users.noreply.github.com>
2024-08-28 22:40:37 +0800

    update

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
+use App\Jobs\InspireJob;
+use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::job( new InspireJob() )
+        ->everyMinute()
+        ->onOneServer();
