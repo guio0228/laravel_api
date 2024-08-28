1cdbab066ec85667fdd4e39a920dfce2242ac07c
guio0228 <93069491+guio0228@users.noreply.github.com>
2024-08-28 22:06:17 +0800

    add testCommand

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Console\Commands;
+use App\Jobs\TestJob;
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    protected $signature = 'app:test-command';
+
+    protected $description = 'Starts a test job.';
+
+    public function handle()
+    {
+        $job = new TestJob();
+        dispatch( $job );
+    }
+}
